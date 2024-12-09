3fd760897c02070f6b21578ad5038e3731ec555f
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-09 22:04:41 +0900

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,22 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    
+    <div className="bg-black bg-home-img bg-cover bg-center">
+      <main>
+        <div>
+          <h1>Ryu's Computer<br />Repair Shop</h1>
+          <address>
+            121, Park Street<br />
+            New York, USA
+          </address>
+          <p>
+            24/7 
+          </p>
+          <Link href="tel:0120-222-222" className="hover:underline">
+          0120-222-222
+          </Link>
+        </div>
+      </main>
+    </div>
   );
 }
