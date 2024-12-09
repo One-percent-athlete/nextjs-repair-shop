e987324eea57c650722191271b7bdad3545ff299
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-09 22:29:15 +0900

    Create template file

--- src/app/(rs)/template.tsx
+++ src/app/(rs)/template.tsx
@@ -1,1 +1,11 @@
-export default 
+export default async function Template({
+    children,
+} : {
+    children: React.ReactNode
+}) {
+    return (
+        <div className="animate-appear">
+            {children}
+        </div>
+    )
+}
