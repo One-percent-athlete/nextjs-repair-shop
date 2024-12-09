64a35ea877dda77e0e65b04926e15798840ea63c
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-09 22:53:05 +0900

    Update nav button and button tsx

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Slot, Slottable } from "@radix-ui/react-slot";
 import { cn } from "@/lib/utils;"
 
-function Button({ asChild, children, leftElement, rightElement, ...props }) {
+export default function Button({ asChild, children, leftElement, rightElement, ...props }) {
     const Comp = asChild ? Slot : "button";
     return (
         <Comp {...props}>
