656f2c935c3398691956c8691a4a28ec8d600f04
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-09 22:27:33 +0900

    Create laypout file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
     <div className="bg-black bg-home-img bg-cover bg-center">
       <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
         <div className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 w/4-5 sm:max-w-96 mx-auto text-white sm:text-2xl">
-          <h1>Ryu's Computer<br />Repair Shop</h1>
+          <h1 className="text-4xl font-bold">Ryu's Computer<br />Repair Shop</h1>
           <address>
             121, Park Street<br />
             New York, USA
           </address>
           <p>
-            24/7 
+            Open 24/7 
           </p>
           <Link href="tel:0120-222-222" className="hover:underline">
           0120-222-222
