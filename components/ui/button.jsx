1224e90cee9134f3f265d4e932449eec6280b96a
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-10 18:47:13 +0900

    Update header and button

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -1,17 +1,14 @@
 // your-button.jsx
 import React from "react";
 import { Slot, Slottable } from "@radix-ui/react-slot";
-import { cn } from "@/lib/utils;"
 
 export default function Button({ variant,
     size,
     label, title, asChild, children, className }) {
     const Comp = asChild ? Slot : "button";
     return (
-        <Comp {...props}>
-            {leftElement}
+        <Comp>
             <Slottable>{children}</Slottable>
-            {rightElement}
         </Comp>
     );
 }
